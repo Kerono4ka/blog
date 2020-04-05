4fe4c436071e0fe1e3655acc488252a4c9e53646
kerono4ka <olgha.tovstenko@gmail.com>
2020-04-06 00:20:21 +0300

    refactor article controller with filters

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -2,6 +2,8 @@ class ArticlesController < ApplicationController
   http_basic_authenticate_with name: "bla", password: "bla",
   except: [:index, :show]
 
+  before_action :set_article, except: [:index, :new, :create]
+
   def index
     if params.has_key? :search
       @search = params[:search]
@@ -13,7 +15,7 @@ class ArticlesController < ApplicationController
   end
 
   def show
-    @article = Article.find(params[:id])
+    # @article = Article.find(params[:id])
   end    
 
   def new
@@ -21,7 +23,7 @@ class ArticlesController < ApplicationController
   end
 
   def edit
-    @article = Article.find(params[:id])
+    # @article = Article.find(params[:id])
   end
 
   def create
@@ -35,8 +37,7 @@ class ArticlesController < ApplicationController
   end
 
   def update
-    @article = Article.find(params[:id])
-
+    # @article = Article.find(params[:id])
     if @article.update(article_params)
       redirect_to @article
     else
@@ -45,13 +46,17 @@ class ArticlesController < ApplicationController
   end
 
   def destroy
-    @article = Article.find(params[:id])
+    # @article = Article.find(params[:id])
     @article.destroy
 
     redirect_to articles_path
   end
 
   private
+  def set_article
+    @article = Article.find(params[:id])
+  end
+
   def article_params
     params.require(:article).permit(:title, :text, :image)
   end
