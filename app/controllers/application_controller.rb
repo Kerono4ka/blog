74a00955eccf35ed414d36cb85e771deb03c2d40
kerono4ka <olgha.tovstenko@gmail.com>
2020-04-08 02:56:01 +0300

    fix commenter avatar issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,7 +8,7 @@ class ApplicationController < ActionController::Base
   protected
   def current_user
     if session[:user_id]
-      @current_user ||= User.find(session[:user_id])
+      @current_user ||= User.find_by(id: session[:user_id])
     else
       @current_user = nil
     end
