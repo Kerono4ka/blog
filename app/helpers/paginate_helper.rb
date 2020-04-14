78f2a03cd7fc704372a1127ef2fe91a09c0d2873
kerono4ka <olgha.tovstenko@gmail.com>
2020-04-14 20:23:37 +0300

    move custom_pagination_render to lib folder

--- app/helpers/paginate_helper.rb
+++ app/helpers/paginate_helper.rb
@@ -1,40 +1,7 @@
 module PaginateHelper
 
-  class CustomPaginateRenderer < WillPaginate::ActionView::LinkRenderer
-    def container_attributes
-      {class: "pagination pagination-sm"}
-    end
-  
-    def page_number(page)
-      if page == current_page
-        "<li class='page-item active' aria-current='page'>"+
-        link(page, page, rel: rel_value(page), class: "page-link")+"</li>"
-      else
-        "<li class='page-item'>"+link(page, page, rel: rel_value(page), class: "page-link")+"</li>"
-      end
-    end
-  
-    def previous_page
-      num = @collection.current_page > 1 && @collection.current_page - 1
-      previous_or_next_page(num, "&laquo;")
-    end
-  
-    def next_page
-      num = @collection.current_page < total_pages && @collection.current_page + 1
-      previous_or_next_page(num, "&raquo;")
-    end
-  
-    def previous_or_next_page(page, text)
-      if page
-        "<li class='page-item'>"+link(text, page, class: "page-link")+"</li>"
-      else
-        "<li class='page-item'><a class='page-link' aria-disabled='true'>"+text+"</a></li>"
-      end
-    end
-  end
-
   def custom_paginate_renderer
-    CustomPaginateRenderer.new
+    Pagination::CustomPaginateRenderer.new
   end
 
 end
